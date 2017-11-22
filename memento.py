c26f4f0fbe58ef2200f21be32f090b67943cf4fd
allyson <allyson.cunha.cunha@hotmail.com>
2017-11-22 00:13:24 -0200

    refatoracao

--- memento.py
+++ memento.py
@@ -1,22 +1,56 @@
 class Memento:
-  def __init__(self,string):
-    self.string = string
-  def getString(self):
-    return self.string
+  estadoTexto = ""
+  def setMemento(self,string):
+    if(string != ""):
+      self.estadoTexto = string
+  def getTextoSalvo(self):
+    return self.estadoTexto
 
 class TakerCareText:
   estados = []
-  def TextoCareTaker(self):
-        estados = []
- 
   def adicionarMemento(self, memento):
-        self.estados.push(memento);
-  
+        self.estados.append(memento)
   def getUltimoEstadoSalvo(self):
-        if (self.estados.size() <= 0):
-            return ""
+        if (len(self.estados) <= 0):
+            return Memento()
         else:
-          estadoSalvo = self.estados.get(estados.size() - 1);
-          self.estados.remove(self.estados.size() - 1);
-          return estadoSalvo;
+          estadoSalvo = self.estados.pop()
+          return estadoSalvo
 
+class Texto:
+    def __init__(self):
+      self.caretaker = TakerCareText()
+      self.texto = ""
+    def escreverTexto(self,novoTexto):
+        memento = Memento()
+        memento.setMemento(self.texto)
+        self.caretaker.adicionarMemento(memento)
+        self.texto += novoTexto
+    def desfazerEscrita(self):
+        self.texto = self.caretaker.getUltimoEstadoSalvo().getTextoSalvo();
+    def mostrarTexto(self):
+        print(self.texto)
+        
+texto = Texto()
+texto.escreverTexto("Primeira linha do texto\n");
+texto.escreverTexto("Segunda linha do texto\n");
+texto.escreverTexto("Terceira linha do texto\n");
+texto.mostrarTexto();
+texto.desfazerEscrita();
+texto.mostrarTexto();
+texto.desfazerEscrita();
+texto.mostrarTexto();
+texto.desfazerEscrita();
+texto.mostrarTexto();
+texto.desfazerEscrita();
+texto.mostrarTexto();
+texto.escreverTexto("Primeira linha do texto\n");
+texto.escreverTexto("Segunda linha do texto\n");
+texto.escreverTexto("Terceira linha do texto\n");
+texto.mostrarTexto();
+texto.escreverTexto("Terceira linha do texto\n");
+texto.mostrarTexto();
+texto.desfazerEscrita();
+texto.mostrarTexto();
+texto.desfazerEscrita();
+texto.mostrarTexto();
